b79388bc3e962cf0b92950f0c829ec210dc61f86
royaldinan <137694530+royaldinan@users.noreply.github.com>
2025-11-08 20:07:40 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finsmart-v1';
+const CACHE_NAME = 'finsmart-v2';
 const urlsToCache = [
   './',
   './index.html'
@@ -45,4 +45,5 @@ self.addEventListener('fetch', event => {
       }
     )
   );
+
 });
